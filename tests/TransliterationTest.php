00c160a2c6938e0667582195cb8633eaf1ce3f78
gregkos <gregkos.dev@gmail.com>
2021-01-06 13:20:28 +0200

    Added some transliteration tests

--- tests/TransliterationTest.php
+++ tests/TransliterationTest.php
@@ -14,4 +14,39 @@ class TransliterationTest extends TestCase
 
         $this->assertEquals('kalimera', $transliterated);
     }
+
+    /** @test */
+    public function transliterate_a_mixed_string()
+    {
+        $transliterated = (new GreekString('Καλημέρα Bob!'))->transliterate();
+
+        $this->assertEquals('Kalimera Bob!', $transliterated);
+    }
+
+    /**
+     * @test
+     * @dataProvider transliterationExamplesProvider
+     */
+    public function transliterate_various_strings($original, $transliterated)
+    {
+        $this->assertEquals($transliterated, (new GreekString($original))->transliterate());
+    }
+
+    public function transliterationExamplesProvider(): array
+    {
+        return [
+            ['αρχικό', 'archiko'],
+            ['διεκπαιρέωση', 'diekpaireosi'],
+            ['κεφαλαιοποίηση', 'kefalaiopoiisi'],
+            ['Αύγουστος', 'Avgoustos'],
+            ['Μαΐου', 'Maiou'],
+            ['ενσυναίσθηση', 'ensynaisthisi'],
+            ['προϋπάρχον', 'proyparchon'],
+            ['προκατάληψη', 'prokatalipsi'],
+            ['ψυγείο', 'psygeio'],
+            ['Δευτέρα', 'Deftera'],
+            ['Ταχίστη αλώπηξ βαφής ψημένη γη, δρασκελίζει υπέρ νωθρού κυνός.', 'Tachisti alopix vafis psimeni gi, draskelizei yper nothrou kynos.'],
+            ['Ξεσκεπάζω την ψυχοφθόρα βδελυγμία.', 'Xeskepazo tin psychofthora vdelygmia.'],
+        ];
+    }
 }
