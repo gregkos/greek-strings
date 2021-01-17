6b8f6b1dda5c1893b80a25341d6bd5e8846b3279
gregkos <gregkos.dev@gmail.com>
2021-01-17 14:40:50 +0200

    Add further tests for upper/lower case conversion

--- src/CaseConverter.php
+++ src/CaseConverter.php
@@ -4,11 +4,6 @@ namespace GregKos\GreekStrings;
 
 class CaseConverter
 {
-    private static array $upper_accent_letters = ['Ά', 'Έ', 'Ή', 'Ί|Ϊ', 'Ό', 'Ύ|Ϋ', 'Ώ'];
-    private static array $upper_no_accent_letters = ['Α', 'Ε', 'Η', 'Ι', 'Ο', 'Υ', 'Ω'];
-    private static array $lower_accent_letters = ['ά', 'έ', 'ή', 'ί|ϊ', 'ό', 'ύ|ϋ', 'ώ'];
-    private static array $lower_no_accent_letters = ['α', 'ε', 'η', 'ι', 'ο', 'υ', 'ω'];
-
     public static function convert(string $str, string $target = null, bool $removeAccent = true): string
     {
         if ($target === 'upper') {
@@ -28,14 +23,19 @@ class CaseConverter
 
     private static function removeAccent(string $str): string
     {
+        $upper_accent_letters = ['Ά', 'Έ', 'Ή', 'Ί|Ϊ|Ϊ́', 'Ό', 'Ύ|Ϋ|Ϋ́', 'Ώ'];
+        $upper_no_accent_letters = ['Α', 'Ε', 'Η', 'Ι', 'Ο', 'Υ', 'Ω'];
+        $lower_accent_letters = ['ά', 'έ', 'ή', 'ί|ϊ|ΐ', 'ό', 'ύ|ϋ|ΰ', 'ώ'];
+        $lower_no_accent_letters = ['α', 'ε', 'η', 'ι', 'ο', 'υ', 'ω'];
+
         // mb_regex_encoding('UTF-8');
 
-        for ($i = 0; $i < count(self::$upper_accent_letters); $i++) {
-            $str = mb_ereg_replace(self::$upper_accent_letters[$i], self::$upper_no_accent_letters[$i], $str);
+        for ($i = 0; $i < count($upper_accent_letters); $i++) {
+            $str = mb_ereg_replace($upper_accent_letters[$i], $upper_no_accent_letters[$i], $str);
         }
 
-        for ($i = 0; $i < count(self::$lower_accent_letters); $i++) {
-            $str = mb_ereg_replace(self::$lower_accent_letters[$i], self::$lower_no_accent_letters[$i], $str);
+        for ($i = 0; $i < count($lower_accent_letters); $i++) {
+            $str = mb_ereg_replace($lower_accent_letters[$i], $lower_no_accent_letters[$i], $str);
         }
 
         return $str;
--- tests/CaseConversionTest.php
+++ tests/CaseConversionTest.php
@@ -22,4 +22,43 @@ class CaseConversionTest extends TestCase
 
         $this->assertEquals('καλημερα', $converted);
     }
+
+    /** @test */
+    public function convert_a_greeting_without_removing_accent()
+    {
+        $converted = (new GreekString('ΚΑΛΗΜΈΡΑ'))->toLower(false);
+
+        $this->assertEquals('καλημέρα', $converted);
+    }
+
+    /**
+     * @test
+     * @dataProvider conversionExamplesProvider
+     */
+    public function convert_various_strings($original, $upper, $lower, $upperWithAccent, $lowerWithAccent)
+    {
+        $this->assertEquals($upper, (new GreekString($original))->toUpper());
+        $this->assertEquals($lower, (new GreekString($original))->toLower());
+        $this->assertEquals($upperWithAccent, (new GreekString($original))->toUpper(false));
+        $this->assertEquals($lowerWithAccent, (new GreekString($original))->toLower(false));
+    }
+
+    public function conversionExamplesProvider(): array
+    {
+        return [
+            ['', '', '', '', ''],
+            ['αρχικό', 'ΑΡΧΙΚΟ', 'αρχικο', 'ΑΡΧΙΚΌ', 'αρχικό'],
+            ['διεκπαιρέωση', 'ΔΙΕΚΠΑΙΡΕΩΣΗ', 'διεκπαιρεωση', 'ΔΙΕΚΠΑΙΡΈΩΣΗ', 'διεκπαιρέωση'],
+            ['κεφαλαιοποίηση', 'ΚΕΦΑΛΑΙΟΠΟΙΗΣΗ', 'κεφαλαιοποιηση', 'ΚΕΦΑΛΑΙΟΠΟΊΗΣΗ', 'κεφαλαιοποίηση'],
+            ['Αύγουστος', 'ΑΥΓΟΥΣΤΟΣ', 'αυγουστος', 'ΑΎΓΟΥΣΤΟΣ', 'αύγουστος'],
+            ['Μαΐου', 'ΜΑΙΟΥ', 'μαιου', 'ΜΑΪ́ΟΥ', 'μαΐου'],
+            ['ενσυναίσθηση', 'ΕΝΣΥΝΑΙΣΘΗΣΗ', 'ενσυναισθηση', 'ΕΝΣΥΝΑΊΣΘΗΣΗ', 'ενσυναίσθηση'],
+            ['προϋπάρχον', 'ΠΡΟΥΠΑΡΧΟΝ', 'προυπαρχον', 'ΠΡΟΫΠΆΡΧΟΝ', 'προϋπάρχον'],
+            ['προκατάληψη', 'ΠΡΟΚΑΤΑΛΗΨΗ', 'προκαταληψη', 'ΠΡΟΚΑΤΆΛΗΨΗ', 'προκατάληψη'],
+            ['ψυγείο', 'ΨΥΓΕΙΟ', 'ψυγειο', 'ΨΥΓΕΊΟ', 'ψυγείο'],
+            ['Δευτέρα', 'ΔΕΥΤΕΡΑ', 'δευτερα', 'ΔΕΥΤΈΡΑ', 'δευτέρα'],
+            ['Ταχίστη αλώπηξ βαφής ψημένη γη, δρασκελίζει υπέρ νωθρού κυνός.', 'ΤΑΧΙΣΤΗ ΑΛΩΠΗΞ ΒΑΦΗΣ ΨΗΜΕΝΗ ΓΗ, ΔΡΑΣΚΕΛΙΖΕΙ ΥΠΕΡ ΝΩΘΡΟΥ ΚΥΝΟΣ.', 'ταχιστη αλωπηξ βαφης ψημενη γη, δρασκελιζει υπερ νωθρου κυνος.', 'ΤΑΧΊΣΤΗ ΑΛΏΠΗΞ ΒΑΦΉΣ ΨΗΜΈΝΗ ΓΗ, ΔΡΑΣΚΕΛΊΖΕΙ ΥΠΈΡ ΝΩΘΡΟΎ ΚΥΝΌΣ.', 'ταχίστη αλώπηξ βαφής ψημένη γη, δρασκελίζει υπέρ νωθρού κυνός.'],
+            ['Ξεσκεπάζω την ψυχοφθόρα βδελυγμία.', 'ΞΕΣΚΕΠΑΖΩ ΤΗΝ ΨΥΧΟΦΘΟΡΑ ΒΔΕΛΥΓΜΙΑ.', 'ξεσκεπαζω την ψυχοφθορα βδελυγμια.', 'ΞΕΣΚΕΠΆΖΩ ΤΗΝ ΨΥΧΟΦΘΌΡΑ ΒΔΕΛΥΓΜΊΑ.', 'ξεσκεπάζω την ψυχοφθόρα βδελυγμία.'],
+        ];
+    }
 }
